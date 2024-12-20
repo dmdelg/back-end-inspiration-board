265af146921e451b3d0ef188e339abc8b16147af
Leidy <leidymarbu@gmail.com>
2024-12-20 17:35:43 -0300

    Backend Setup Done

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,22 @@
 from flask import Flask
-from flask_cors import CORS
+from .db import db, migrate
 import os
-# Import models, blueprints, and anything else needed to set up the app or database
-
+from .models import board, card
+from flask_cors import CORS
 
 def create_app(config=None):
     app = Flask(__name__)
 
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/inspiration_board_development'
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
 
     if config:
         app.config.update(config)
 
     # Initialize app with SQLAlchemy db and Migrate
+    db.init_app(app)
+    migrate.init_app(app,db)
 
     # Register Blueprints 
 
