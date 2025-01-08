a8c50374c975a414fcfe1c26ee992a78d00fba1a
Liubov Dav <lubasha0512@gmail.com>
2025-01-07 17:08:21 -0800

    Added new endpoint

--- seed_card.py
+++ seed_card.py
@@ -4,23 +4,23 @@ from app.models.card import Card
 my_app = create_app()
 with my_app.app_context():
     # Team 1: Team Tiger
-    db.session.add(Card(board_id=8, message="Optimize database queries for better performance."))
-    db.session.add(Card(board_id=8, message="Implement user authentication and authorization."))
-    db.session.add(Card(board_id=8, message="Develop new REST API for order processing."))
+    db.session.add(Card(board_id=16, message="Optimize database queries for better performance."))
+    db.session.add(Card(board_id=16, message="Implement user authentication and authorization."))
+    db.session.add(Card(board_id=16, message="Develop new REST API for order processing."))
 
     # Team 2: Rainbow team
-    db.session.add(Card(board_id=9, message="Refactor the dashboard for better responsiveness."))
-    db.session.add(Card(board_id=9, message="Integrate the payment gateway UI."))
-    db.session.add(Card(board_id=9, message="Fix layout issues on the mobile view."))
+    db.session.add(Card(board_id=17, message="Refactor the dashboard for better responsiveness."))
+    db.session.add(Card(board_id=17, message="Integrate the payment gateway UI."))
+    db.session.add(Card(board_id=17, message="Fix layout issues on the mobile view."))
 
     # Team 3: Blue Ocean team
-    db.session.add(Card(board_id=10, message="Develop a real-time chat feature."))
-    db.session.add(Card(board_id=10, message="Ensure the application is compliant with security standards."))
-    db.session.add(Card(board_id=10, message="Set up CI/CD pipelines for automated deployment."))
+    db.session.add(Card(board_id=18, message="Develop a real-time chat feature."))
+    db.session.add(Card(board_id=18, message="Ensure the application is compliant with security standards."))
+    db.session.add(Card(board_id=18, message="Set up CI/CD pipelines for automated deployment."))
 
     # Team 4: My team
-    db.session.add(Card(board_id=11, message="Set up monitoring and alerting for critical services."))
-    db.session.add(Card(board_id=11, message="Migrate the application to Kubernetes."))
-    db.session.add(Card(board_id=11, message="Automate server backups to cloud storage."))
+    db.session.add(Card(board_id=19, message="Set up monitoring and alerting for critical services."))
+    db.session.add(Card(board_id=19, message="Migrate the application to Kubernetes."))
+    db.session.add(Card(board_id=19, message="Automate server backups to cloud storage."))
 
     db.session.commit()
